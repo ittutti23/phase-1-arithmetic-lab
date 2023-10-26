2578a6bcbddcafbb40fb49f5f2c316ae30cb34ae
Peyton <1209942pl@gmail.com>
2023-10-26 18:51:25 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+let num1 = 31
+let num2 = 2
+let multiply = (num1 * num2)
+//
+let random = Math.floor(Math.random() * 100) + 1;
+//
+let num3 = 204;
+let num4 = 100;
 
-// Write your code here
+let mod = num3 % num4;
+console.log(mod); 
+//
+let num5 = 3;
+let num6 = 12;
+let num7 = 5;
+let num8 = 20;
+
+let max = Math.max(num5, num6, num7, num8);
+console.log(max);
